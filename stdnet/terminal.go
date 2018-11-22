833e7b8f8c1c8ee83242492952b67ac3515a913f
Anders Liu <yangl@microsoft.com>
2018-11-21 16:45:02 -0800

    dep ensure

--- stdnet/terminal.go
+++ stdnet/terminal.go
@@ -3,24 +3,13 @@ package main
 import (
 	"log"
 	"os"
-	"syscall"
 
 	"golang.org/x/crypto/ssh/terminal"
 )
 
 var (
-	dll            = syscall.MustLoadDLL("kernel32")
-	setConsoleMode = dll.MustFindProc("SetConsoleMode")
-	memory         uint32
+	state terminalState
 )
-
-func setInputConsoleMode(h syscall.Handle, m uint32) error {
-	r, _, err := setConsoleMode.Call(uintptr(h), uintptr(m))
-	if r == 0 {
-		return err
-	}
-	return nil
-}
 
 type terminalState struct {
 	state *terminal.State
@@ -31,29 +20,15 @@ func isTerminal(fd uintptr) bool {
 }
 
 func setConsoleRaw() {
-	h := syscall.Handle(os.Stdin.Fd())
+	s, err := terminal.MakeRaw(int(os.Stdin.Fd()))
 
-	if err := syscall.GetConsoleMode(h, &memory); err != nil {
+	if err != nil {
 		log.Fatal(err)
 	}
-	if err := setInputConsoleMode(h, 0); err != nil {
-		log.Fatal(err)
-	}
+
+	state.state = s
 }
 
 func restoreConsole() {
-	h := syscall.Handle(os.Stdin.Fd())
-	setInputConsoleMode(h, memory)
-}
-
-func makeRaw(fd uintptr) (*terminalState, error) {
-	state, err := terminal.MakeRaw(int(fd))
-
-	return &terminalState{
-		state: state,
-	}, err
-}
-
-func restore(fd uintptr, oldState *terminalState) error {
-	return terminal.Restore(int(fd), oldState.state)
+	terminal.Restore(int(os.Stdin.Fd()), state.state)
 }
